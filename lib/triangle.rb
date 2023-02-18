d95d694777d3d5240d85c0516b51bed14eeb35ae
pmihango <pmihango@gmail.com>
2023-02-18 14:57:21 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+  end
+  
+  def initialize(side_x, side_y, side_z)
+    @side_x = side_x
+    @side_y = side_y
+    @side_z = side_z
+  end
+  
+  def kind
+    # Check if the triangle is valid
+    if @side_x <= 0 || @side_y <= 0 || @side_z <= 0 || (@side_x + @side_y <= @side_z) || (@side_y + @side_z <= @side_x) || (@side_z + @side_x <= @side_y)
+      raise TriangleError.new("It is not a valid triangle")
+    end
+    
+    # Determine the kind of triangle
+    if @side_x == @side_y && @side_y == @side_z
+      return :equilateral
+    elsif @side_x == @side_y || @side_x == @side_z || @side_y == @side_z
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
 end
